1e1458f69c9bea963fb981a1000b98d9412ec1c4
FFFomalhaut <chenjinyang0511@gmail.com>
2023-09-01 21:23:37 +0800

    修改登录方式。使用setInterval，两次提交请求间隔若干秒，以期避免登录顺序混乱问题。

--- Multilogin.js
+++ Multilogin.js
@@ -18,30 +18,39 @@
 
     var from = $("input#from").val();
     from = from ? from : "/bbs/index";
-    var users = {
-        "归尘":         "99b3209963cec83db54a48d1c9b1dd31",
-        "歸塵":         "99b3209963cec83db54a48d1c9b1dd31",
-        "2022飞行团":   "7f901d1e7bd1a1b5bc76061a4ff246b1",
-        "22飞行团":     "7f901d1e7bd1a1b5bc76061a4ff246b1",
-        "实践部":       "c5af0543407a6e8ba11db6a69f792014",
-        "组织部":       "a1655b339c48a150da4809b3b1199dde",
-        "23石花洞":     "f03c726ec264d1bc56cff2e72fba0d48"
-    };
+    var users = [
+        ["归尘",        "99b3209963cec83db54a48d1c9b1dd31"],
+        ["歸塵",        "99b3209963cec83db54a48d1c9b1dd31"],
+        ["2022飞行团",  "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+        ["22飞行团",    "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+        ["实践部",      "c5af0543407a6e8ba11db6a69f792014"],
+        ["组织部",      "a1655b339c48a150da4809b3b1199dde"],
+        ["23石花洞",    "f03c726ec264d1bc56cff2e72fba0d48"]
+    ];
 
     var btn = $("<input type='button' value='全部登录' class='button' onclick='multilogin();'></input>");
     $("input.button:first").before(btn,"\n");
     $("input.button:first")[0].previousSibling.data=("\n    \n");
     $("input.button:last")[0].nextSibling.data=("\n    \n");
 
-
     window.multilogin = function() {
-        for (var x in users) {
-            $.post("action.php",{
-                username:x,
-                password1:users[x]
-            })
+        var index = users.length;
+        function Multilogin_single() {
+            // console.log(index);
+            index--;
+            if (index == -1) {
+                clearInterval(Interval_Multilogin);
+                window.location = from;
+            } else {
+                var [usrn, pswd] = users[index];
+                $.post("action.php",{
+                    username:usrn,
+                    password1:pswd,
+                })
+                // console.log(usrn,pswd);
+            }
         }
-        window.location=from;
+        var Interval_Multilogin = setInterval(Multilogin_single, 2000);
     };
 
     // Your code here...
