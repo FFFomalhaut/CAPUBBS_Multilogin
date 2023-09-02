be37f44b94de8078ffbecc8538262697ee750c65
FFFomalhaut <chenjinyang0511@gmail.com>
2023-09-02 17:47:53 +0800

    消除了“getUsers函数未定义”的警告；更改了author名；删除了之前未删除只是注释的账号密码。

--- Multilogin.js
+++ Multilogin.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.2.3
 // @description  修改了fromdesktop的判断方式
-// @author       
+// @author       FFFomalhaut
 // @match        https://*.chexie.net/bbs/login/*
 // @icon         https://chexie.net/assets/images/capu.jpg
 // @grant        none
@@ -15,19 +15,10 @@
 (function() {
     'use strict';
     /* eslint-disable no-multi-spaces */
-    /* globals $ */
+    /* globals $, getUsers */
 
     var from = $("input#from").val();
     from = from ? from : "/bbs/index";
-//    var users = [
-//        ["归尘",        "99b3209963cec83db54a48d1c9b1dd31"],
-//        ["歸塵",        "99b3209963cec83db54a48d1c9b1dd31"],
-//        ["2022飞行团",  "7f901d1e7bd1a1b5bc76061a4ff246b1"],
-//        ["22飞行团",    "7f901d1e7bd1a1b5bc76061a4ff246b1"],
-//        ["实践部",      "c5af0543407a6e8ba11db6a69f792014"],
-//        ["组织部",      "a1655b339c48a150da4809b3b1199dde"],
-//        ["23石花洞",    "f03c726ec264d1bc56cff2e72fba0d48"]
-//    ];
     var users = getUsers();
 
     var btn = $("<input type='button' value='全部登录' class='button' onclick='multilogin();'></input>");
