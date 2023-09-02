4221357a26fd32e34205c65d4f9850cbdc3111d7
FFFomalhaut <chenjinyang0511@gmail.com>
2023-09-02 19:30:17 +0800

    增加了getUsers.sample.js

---
+++ getUsers.sample.js
@@ -0,0 +1,8 @@
+function getUsers() {
+    var users = [
+        ["归尘",        "d41d8cd98f00b204e9800998ecf8427e"],
+        ["Example",     "FillInYourEncryptedPasswordHere!"]
+    ];
+    return users;
+}
+// Fill in the password encrypted with md5.
