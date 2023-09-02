076b298dc9d80b38226b4bea7acfa85087ac32f9
FFFomalhaut <chenjinyang0511@gmail.com>
2023-09-02 17:44:48 +0800

    账号密码数据和代码分离，被保存到本地

--- Multilogin.js
+++ Multilogin.js
@@ -3,11 +3,12 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.2.3
 // @description  修改了fromdesktop的判断方式
-// @author       To Dust
+// @author       
 // @match        https://*.chexie.net/bbs/login/*
 // @icon         https://chexie.net/assets/images/capu.jpg
 // @grant        none
 // @require      https://chexie.net/assets/js/jquery.min.js
+// @require	 file:///C:/Users/admin/Desktop/PlayWithBBS/Multilogin/getUsers.js
 
 // ==/UserScript==
 
@@ -18,15 +19,16 @@
 
     var from = $("input#from").val();
     from = from ? from : "/bbs/index";
-    var users = [
-        ["归尘",        "99b3209963cec83db54a48d1c9b1dd31"],
-        ["歸塵",        "99b3209963cec83db54a48d1c9b1dd31"],
-        ["2022飞行团",  "7f901d1e7bd1a1b5bc76061a4ff246b1"],
-        ["22飞行团",    "7f901d1e7bd1a1b5bc76061a4ff246b1"],
-        ["实践部",      "c5af0543407a6e8ba11db6a69f792014"],
-        ["组织部",      "a1655b339c48a150da4809b3b1199dde"],
-        ["23石花洞",    "f03c726ec264d1bc56cff2e72fba0d48"]
-    ];
+//    var users = [
+//        ["归尘",        "99b3209963cec83db54a48d1c9b1dd31"],
+//        ["歸塵",        "99b3209963cec83db54a48d1c9b1dd31"],
+//        ["2022飞行团",  "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+//        ["22飞行团",    "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+//        ["实践部",      "c5af0543407a6e8ba11db6a69f792014"],
+//        ["组织部",      "a1655b339c48a150da4809b3b1199dde"],
+//        ["23石花洞",    "f03c726ec264d1bc56cff2e72fba0d48"]
+//    ];
+    var users = getUsers();
 
     var btn = $("<input type='button' value='全部登录' class='button' onclick='multilogin();'></input>");
     $("input.button:first").before(btn,"\n");
---
+++ getUsers.js
@@ -0,0 +1,14 @@
+function getUsers() {
+    var users = [
+        ["归尘",        "99b3209963cec83db54a48d1c9b1dd31"],
+        ["歸塵",        "99b3209963cec83db54a48d1c9b1dd31"],
+        ["2022飞行团",  "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+        ["22飞行团",    "7f901d1e7bd1a1b5bc76061a4ff246b1"],
+        ["实践部",      "c5af0543407a6e8ba11db6a69f792014"],
+        ["组织部",      "a1655b339c48a150da4809b3b1199dde"],
+        ["23石花洞",    "f03c726ec264d1bc56cff2e72fba0d48"]
+    ];
+    return users;
+}
+
+
