d76ff2bd13606be96532d9c146768a8c9ec55b96
FFFomalhaut <chenjinyang0511@gmail.com>
2024-12-20 13:58:40 +0800

    改进sample中的注释

--- users.sample.js
+++ users.sample.js
@@ -2,6 +2,7 @@ var users = [
     ["Username1",   "TheFirstPasswordEncryptedWithMD5"],
     ["Username2",   "TheSecndPasswordEncryptedWithMD5"]
 ];
-// Fill in the password encrypted with md5.
+// Fill in the password encrypted with md5 (in lowercase).
+// Your main account should be the first.
 // When adding users, don't forget the comma after the square brackets.
 // Then change the file name to users.js
